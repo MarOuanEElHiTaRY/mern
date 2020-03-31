b9452b76de46f5cc337d613a1239e7844450c32b
EL HITARY <elhitarymarouane@gmail.com>
2020-03-31 11:00:35 +0200

    Update messageModel.js

--- backend/models/messageModel.js
+++ backend/models/messageModel.js
@@ -1,10 +1,14 @@
-import { Shema } from 'mongoose';
-const messageShema = new Shema ({
-  msg:{
-    type:string,
+import { Schema } from 'mongoose';
+
+const messageSchema = new Schema({
+  msg: {
+    type: String,
     required: true
-    },
-    userName: { type:Shema.Types.ObjectId, ref: 'User' }
-    });
-    
-    export default messageShema;
+  },
+  userName: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }
+});
+
+export default messageSchema;
